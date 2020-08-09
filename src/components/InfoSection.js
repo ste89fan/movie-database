500eb356863e1c209e95e1f910c8f01df26cb1f3
Stefan <stefan.arbutina@chainapp.tech>
2020-08-09 16:19:12 +0200

    finished

--- src/components/InfoSection.js
+++ src/components/InfoSection.js
@@ -1,29 +1,40 @@
 import React from "react";
+import { Container, Row, Col } from "reactstrap";
 
 const InfoSection = (props) => {
   return (
     <div className="infoSection">
-      <h4>{props.title}</h4>
+      <h4 className="infoSectionTitle">{props.title}</h4>
 
-      <section>
-        <label className="infoLabel">year : </label>
-        <span>{props.year}</span>
-      </section>
+      <Container>
+        <Row className="infoFields">
+          <Col>
+            <label className="infoLabel">year : </label>
+            <span>{props.year}</span>
+          </Col>
+        </Row>
 
-      <section>
-        <label className="infoLabel">plot :</label>
-        <p>{props.plot}</p>
-      </section>
+        <Row className="infoFields">
+          <Col>
+            <label className="infoLabel">plot :</label>
+            <p>{props.plot}</p>
+          </Col>
+        </Row>
 
-      <section>
-        <label className="infoLabel">genre :</label>
-        <p>{props.genre}</p>
-      </section>
+        <Row className="infoFields">
+          <Col>
+            <label className="infoLabel">genre :</label>
+            <p>{props.genre}</p>
+          </Col>
+        </Row>
 
-      <section>
-        <label className="infoLabel">actors :</label>
-        <p>{props.actors}</p>
-      </section>
+        <Row className="infoFields">
+          <Col>
+            <label className="infoLabel">actors :</label>
+            <p>{props.actors}</p>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
